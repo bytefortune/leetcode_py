96e5c66400fd2bf8a35b9b8bb7f5576b7151f81a
fortunef <ssfhqsz1@126.com>
2018-09-29 11:08:55 +0800

    middleNode

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.append(os.path.dirname(os.getcwd()))
-from solution.projectionArea import ProjectionArea
+from solution import middleNode
 
 
 class Run(object):
@@ -12,9 +12,21 @@ class Run(object):
         pass
 
     def run_interface(self):
-        obj = ProjectionArea()
-        res = obj.projectionArea([[2,2,2],[2,1,2],[2,2,2]])
-        print(res)
+        self.initlist([1, 2, 3, 4, 5])
+        obj = middleNode.Solution()
+        res = obj.middleNode(self.head)
+        print(res.val)
+
+    def initlist(self, data):
+
+        self.head = middleNode.ListNode(data[0])
+
+        p = self.head
+
+        for i in data[1:]:
+            node = middleNode.ListNode(i)
+            p.next = node
+            p = p.next
 
 
 if __name__ == '__main__':
