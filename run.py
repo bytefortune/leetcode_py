0c826eb401d5873af91b889c27a05d8c37a178b6
fortunef <ssfhqsz1@126.com>
2018-10-02 14:27:06 +0800

    removeElement

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.append(os.path.dirname(os.getcwd()))
-from solution import leafSimilar
+from solution import removeElement
 
 
 class Run(object):
@@ -12,10 +12,8 @@ class Run(object):
         pass
 
     def run_interface(self):
-        obj = leafSimilar.Solution(
-            [3, 5, 1, 6, 2, 9, 8, None, None, 7, 4],
-            [3, 5, 1, 6, 7, 4, 2, None, None, None, None, None, None, 9, 8])
-        res = obj.leafSimilar(obj.node_list1[0], obj.node_list2[0])
+        obj = removeElement.Solution()
+        res = obj.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
         print(res)
 
 
---
+++ solution/removeElement.py
@@ -0,0 +1,82 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# import os
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.append(os.path.dirname(os.getcwd()))
+"""
+给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+示例 1:
+
+给定 nums = [3,2,2,3], val = 3,
+
+函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+
+注意这五个元素可为任意顺序。
+
+你不需要考虑数组中超出新长度后面的元素。
+说明:
+
+为什么返回数值是整数，但输出的答案是数组呢?
+
+请注意，输入数组是以“引用”方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+你可以想象内部操作如下:
+
+// nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+int len = removeElement(nums, val);
+
+// 在函数里修改输入数组对于调用者是可见的。
+// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # ptail = -1
+        # for i, v in enumerate(nums):
+        #     while v == val and i <= len(nums) + ptail:
+        #         nums[i], nums[ptail] = nums[ptail], nums[i]
+        #         v = nums[i]
+        #         ptail -= 1
+        # nums = nums[:len(nums) + ptail + 1]
+        # return len(nums)
+
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i], nums[n - 1] = nums[n - 1], nums[i]
+                n -= 1
+            else:
+                i += 1
+        # return nums[:n]
+        return n
+
+
+# if __name__ == '__main__':
+#     run = Run()
+#     run.run_interface()
